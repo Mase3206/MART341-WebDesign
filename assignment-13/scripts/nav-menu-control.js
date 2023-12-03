c5321fb5f013d677210686de92acd55060f5d4c1
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2023-12-03 14:38:14 -0700

    a13 done?

--- assignment-13/scripts/nav-menu-control.js
+++ assignment-13/scripts/nav-menu-control.js
@@ -5,23 +5,10 @@ function toggleNavMenu() {
 	var footer = document.getElementById("footer");
 
 	if (navMenu.style.display === "none" || navMenu.style.display === "") {
-		navMenu.style.display = "block";		// show #nav-menu
+		navMenu.style.display = "block";	// show #nav-menu
 		menuButton.style.backgroundColor = "#832629";
 	} else {
 		navMenu.style.display = "none";		// show #nav-menu
 		menuButton.style.backgroundColor = "inherit";
 	}
 };
-
-
-function toggleSection(sectionID) {
-	// by making the sectionID be an argument, I can make one function and use it for all the sections
-	var section = document.getElementById(sectionID);
-
-	if (section.style.display === "none" || section.style.display == "") {
-		section.style.display = "block";		// show sectionID
-	} else {
-		section.style.display = "none";			// hide sectionID
-	}
-}
-
